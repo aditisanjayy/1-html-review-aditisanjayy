a3300bad85cc4fb2fc3f2986476a7ad2ff2353a8
Aditi Sanjay <aditisanjay5@gmail.com>
2021-10-25 22:50:01 -0400

    edit and delete function is working

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -13,6 +13,7 @@
             const d = new Intl.NumberFormat("en-US").format(n);
             return "$ " + d;
         },
+
         fetchBookData() {
             fetch('/api/books/')
             .then(response => response.json())
@@ -25,10 +26,18 @@
             })
         },
 
+        postOffer(evt) {
+            if (this.selectedOffer === null) {
+                this.postNewOffer(evt);
+            } else {
+                this.postEditOffer(evt);
+            }
+          },
+
         postNewOffer(evt) {
             // this.offerForm.studentId = this.selectedStudent.id;        
-            console.log("Posting:", this.offerForm);
-             alert("Posting!");
+            // console.log("Posting:", this.offerForm);
+            //  alert("Posting!");
     
             fetch('api/books/create.php', {
                 method:'POST',
@@ -44,12 +53,78 @@
                 this.books = json;
                 
                 // reset the form
-                this.offerForm = {};
+                this.resetOfferForm();
+
+              })
+              
+              .catch( err => {
+                alert("Please fill the requirements!");
               });
+          },
+
+          postEditOffer(evt) {
+            // this.offerForm.studentId = this.selectedStudent.id;
+            this.offerForm.id = this.selectedOffer.id;
+    
+            // console.log("Updating!", this.offerForm);
+    
+            fetch('api/books/update.php', {
+                method:'POST',
+                body: JSON.stringify(this.offerForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+    
+                // reset the form
+                this.resetOfferForm();
+              });
+          },
+
+          postDeleteOffer(o) {
+            if (!confirm("Are you sure you want to delete the offer from "+o.id+"?")) {
+              return;
+            }
+            console.log("Delete!", o);
+    
+            fetch('api/books/delete.php', {
+                method:'POST',
+                body: JSON.stringify(o),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+    
+                // reset the form
+                this.resetOfferForm();
+              });
+          },
+          
+          selectOfferToEdit(o) {
+              this.selectedOffer = o;
+              this.offerForm = Object.assign({}, this.selectedOffer);
+          },
+
+          resetOfferForm() {
+              this.selectedOffer = null;
+              this.offerForm = {};
           }
-  },
+      },
+
     created(){
+
         this.fetchBookData();
+
     }
 }
 Vue.createApp(SomeApp).mount('#anotherApp');
